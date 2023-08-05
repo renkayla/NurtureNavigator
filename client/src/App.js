28242f976e39b8093d2c0e06d7d36b14fd008240
renkayla <116777951+renkayla@users.noreply.github.com>
2023-08-05 15:47:15 -0400

    edited server.js to serve up static assets instead of non-existant clien/images

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 import { Provider } from 'react-redux';
 import store from './utils/store';
 
 import Home from './pages/Home';
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import Header from './components/Header';
+import Footer from './components/Footer';
 
+// Move ApolloClient initialization outside the App component
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: 'http://localhost:3001/graphql', // backend URL
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -35,26 +31,20 @@ const client = new ApolloClient({
 });
 
 function App() {
+  // Use the location to determine whether to show the header
+  const showHeader = window.location.pathname !== '/login' && window.location.pathname !== '/signup';
+
   return (
     <ApolloProvider client={client}>
       <Router>
         <>
           <Provider store={store}>
-            <Header />
+            {showHeader && <Header />} {/* Render the header conditionally */}
             <Routes>
-              <Route 
-                path="/" 
-                element={<Home />} 
-              />
-              {/* <Route 
-                path="/login" 
-                element={<Login />} 
-              />
-              <Route 
-                path="/signup" 
-                element={<Signup />} 
-              /> */}
+              <Route path="/" element={<Home />} />
+              {/* Add other routes */}
             </Routes>
+            <Footer /> {/* Render the footer on all pages */}
           </Provider>
         </>
       </Router>
