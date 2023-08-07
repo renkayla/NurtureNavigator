84223d375d9ff2de140d5157445970b3a379fc51
CaurissaN <caurissanev@gmail.com>
2023-08-07 11:44:36 -0400

    fixed login issue with jwt

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -100,6 +100,7 @@ export default function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
+              {AuthService.loggedIn() ? (
               <div className="hidden lg:ml-4 lg:block">
                 <div className="flex items-center">
                   <button
@@ -178,6 +179,7 @@ export default function Navbar() {
                   </Menu>
                 </div>
               </div>
+              ) : (<></>)}
             </div>
           </div>
 
